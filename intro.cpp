ae75cc393ed384630eba40e6770b50b2af759836
John Docter <143656896+jtdocter27@users.noreply.github.com>
2025-03-10 16:29:04 -0600

    Add files via upload

---
+++ intro.cpp
@@ -0,0 +1,7 @@
+#include <iostream> //python equivalent of import library
+
+int main() { //for starts, // is the python equivalent of #. int main decalres the main function, 
+std::cout << "I am\n"; //std::cout is a predefined object from the iostream library. Outputs data, like the print function
+std::cout << "a bug\n";
+return 0;
+}
